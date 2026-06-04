7ef57e7e51c89f3cfc9d5f7917e525f8691446a7
fats-cmd <okpararonald0@gmail.com>
2026-06-04 23:37:48 +0100

    just learn rust

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,17 @@
 import "./App.css";
+import gsap from "gsap"
+import { useGSAP } from "@gsap/react";
 
-import { motion } from "motion/react";
+
+
 const App = () => {
+  gsap.registerPlugin(useGSAP);// register any plugins, including the useGSAP hook
+  useGSAP(() => { 
+    gsap.to(".box", {x: 200, rotation: 360, duration: 2})
+  })
   return (
     <>
-      {/* fade in animation */}
-      <motion.div
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        transition={{ duration: 2 }}
-        className="w-50 h-50  flex justify-center items-center min-h-screen min-w-screen "
-      >
-        <motion.h1
-          initial={{ opacity: 0, y: -500 }}
-          animate={{ opacity: 1, y: 0 }}
-          className="text-3xl font-bold text-green-300"
-          transition={{ duration: 2, ease: "easeInOut" }}
-        >
-          Hello world
-        </motion.h1>
-      </motion.div>
-
-      {/* hovering and tapping animations */}
-      <motion.div className=" min-w-screen min-h-screen flex justify-center justify-items-center bg-blue-600 rounded-md">
-        <motion.button
-          whileHover={{ scale: 1.1, backgroundColor: "#34D399", y: -10 }}
-          whileTap={{ scale: 0.9, backgroundColor: "#10B981" }}
-          transition={{
-            // duration: 2,
-            type: "spring",
-            stiffness: 900,
-            damping: 0,
-          }}
-          className="text-white font-bold py-2 px-4 rounded-md w-50 h-20"
-        >
-          Hover and Tap Me
-        </motion.button>
-      </motion.div>
-
-      {/* variance and stagger animations */}
+      <div className="box w-30 h-30 bg-red-500"></div>
     </>
   );
 };
